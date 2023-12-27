972dfc84ce5ee2cdef6622456cc24a825c990341
Shokhrukh <122863833+ShokhrukhDeveloper1@users.noreply.github.com>
2023-12-28 02:19:33 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+//1. String ustida amallar
+
+// string str = "teLefOn";
+// Console.Write("x=");
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// if (x > str.Length)
+// {
+//   Console.WriteLine(str.ToUpper());
+// }
+// else
+// {
+//   Console.WriteLine(str.ToLower());
+// }
+
+//2. if-else
+//int x = 10;
+//int y = 15;
+
+//if (x > y)
+//    Console.WriteLine("x is greater than y");
+//else
+//{
+//    if (x < y)
+//        Console.WriteLine("x is less than y");
+//    else
+//    {
+//        if (x == y)
+//            Console.WriteLine("x is equal to y");
+//        else
+//            Console.WriteLine("x and y are not comporable");
+//    }
+//}
+
+//3. Translate
+
+// Console.WriteLine("Quyidagi raqamlardan birini tanlang:\n1. Dushanba " +
+// "2. Seshanba 3. Chorshanba 4. Payshanba 5. Juma 6. Shanba 7. Yakshanba");
+// string x=Console.ReadLine();
+
+// string English = x switch
+// {
+//   "1" => "Monday",
+//   "2" => "Tuesday",
+//   "3" => "Wednesday",
+//   "4" => "Thursday",
+//   "5" => "Friday",
+//   "6" => "Saturday",
+//   "8" => "Sunday",
+//   _ => "operation not found!",
+// };
+// System.Console.WriteLine(English);
